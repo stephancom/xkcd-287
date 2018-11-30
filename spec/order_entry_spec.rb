f91f7870e50c959abbfb60cd03b9632a082e5e31
stephan.com <stephan@stephan.com>
2018-11-30 02:46:58 -0500

    100% coverage
    which, of course, is not 100% tested

---
+++ spec/order_entry_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe OrderEntry do
+  # source: https://www.reuters.com/article/us-dessert/new-yorks-25000-dessert-sets-guinness-record-idUSN0753679220071107
+  let(:name) { 'Frrozen Haute Chocolate' }
+  let(:price) { Money.from_amount(25_000) }
+  let(:item) { Item.new(name,price.format) }
+
+  describe 'a basic order entry' do
+    let(:quantity) { 2 }
+    let(:entry) { OrderEntry.new(item,quantity) }
+
+    it 'computes a subtotal' do
+      expect(entry.subtotal).to eq(price * quantity)
+    end
+
+    describe 'quantity' do    
+      it 'decreases' do
+        expect {
+          entry.quantity = 1
+        }.to change { entry.quantity }.from(2).to(1)
+      end
+
+      it 'increases' do
+        expect {
+          entry.quantity = 10
+        }.to change { entry.quantity }.from(2).to(10)
+      end
+
+      it 'changes subtotal when it changes' do
+        expect {
+          entry.quantity += 3
+        }.to change { entry.subtotal }.from(price * quantity).to(price * (quantity + 3))
+      end
+
+      it 'fails when it becomes 0' do
+        expect {
+          entry.quantity = 0
+        }.to raise_error(OrderEntry::InvalidQuantity)
+      end
+
+      it 'fails when it becomes negative' do
+        expect {
+          entry.quantity -= 3
+        }.to raise_error(OrderEntry::InvalidQuantity)
+      end
+    end
+
+    describe 'returned row' do
+      let(:row) { entry.to_row }
+
+      it 'has the quantity' do
+        expect(row.first).to eq(quantity)
+      end
+
+      it 'has the name' do
+        expect(row[1]).to eq(name)
+      end
+
+      it 'has the formatted subtotal' do
+        expect(row.last).to eq( (price*quantity).format )
+      end
+    end
+
+    describe 'string representation' do
+      let(:string) { entry.to_s }
+      let(:name_expression) { /#{Regexp.quote(name)}/ }
+      let(:subtotal_expression) { /#{Regexp.quote((price * quantity).format)}/ }
+
+      it 'has the name' do
+        expect(string).to match(name_expression)
+      end
+
+      it 'has the formatted subtotal' do
+        expect(string).to match(subtotal_expression)
+      end
+    end
+  end
+
+  describe 'constructing' do
+    it 'fails when quantity is 0' do
+      expect {
+        OrderEntry.new(item, 0)
+      }.to raise_error(OrderEntry::InvalidQuantity)
+    end
+
+    it 'fails when quantity is negative' do
+      expect {
+        OrderEntry.new(item, -1)
+      }.to raise_error(OrderEntry::InvalidQuantity)
+    end
+  end
+end
---
+++ spec/order_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+require 'pry'
+
+describe Order do
+  # source: https://youtu.be/iuE_a1pTsO4
+  # yeah, yeah, prices ought to be in pounds.  i18n wasn't in the spec.
+  let(:menu) { Menu.new('Frog and Peach') }
+  let(:fp_price) { Money.from_amount(12.34) }
+  let(:frog_a_la_peche) { Item.new('frog a la peche', fp_price.format) }
+  let(:pf_price) { Money.from_amount(44.33) }
+  let(:peche_a_la_frog) { Item.new('peche a la frog', pf_price.format) }
+  let(:mint_price) { Money.from_amount(0.25) }
+  let(:mint) { Item.new('after dinner mint', mint_price.format) }
+  before do
+    menu << frog_a_la_peche
+    menu << peche_a_la_frog
+    menu << mint
+  end
+
+  describe 'an order' do
+    let(:order) { Order.new }
+    before do
+      order << frog_a_la_peche
+      order << peche_a_la_frog
+    end
+
+    it 'should have the right number of items' do
+      expect(order.items_count).to eq(2)
+    end
+
+    it 'should have the right total' do
+      expect(order.total).to eq(fp_price + pf_price)
+    end
+
+    describe 'adding one new item' do
+      it 'should have the right number of items' do
+        expect {
+          order << mint
+        }.to change { order.items_count }.from(2).to(3)
+      end
+
+      it 'should have the right total' do
+        expect {
+          order << mint
+        }.to change { order.total }.by(mint_price)
+      end
+    end
+
+    describe 'adding multiples of one new item' do
+      it 'should have the right number of items' do
+        expect {
+          order.add_item(mint, 3)
+        }.to change { order.items_count }.from(2).to(5)
+      end
+
+      it 'should have the right total' do
+        expect {
+          order.add_item(mint, 4)
+        }.to change { order.total }.by(mint_price * 4)
+      end
+    end
+
+    describe 'adding an existing item' do
+      it 'should have the right number of items' do
+        expect {
+          order << frog_a_la_peche
+        }.to change { order.items_count }.from(2).to(3)
+      end
+
+      it 'should have the right total' do
+        expect {
+          order << frog_a_la_peche
+        }.to change { order.total }.by(fp_price)
+      end
+
+      describe 'twice' do
+        before do
+          order << peche_a_la_frog
+          order << peche_a_la_frog          
+        end
+
+        it 'should have the right number of items' do
+          expect(order.items_count).to eq(4)          
+        end
+
+        it 'should have the right total' do
+          expect(order.total).to eq(pf_price * 3 + fp_price)          
+        end
+
+        # yeah, it's bad form to inspect internal state, I'm torn whether to expose this or not
+        it 'should have the right number of entries' do
+          expect(order.instance_variable_get(:@entries).count).to eq(2)
+        end
+      end
+    end
+
+    describe 'the table returned by inspect' do
+      subject { order.inspect }
+      it { is_expected.to match(/frog a la peche/) }
+      it { is_expected.to match(/peche a la frog/) }
+      it { is_expected.to match(/\$56.67/) }
+    end
+  end
+end
