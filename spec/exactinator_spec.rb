bd528379ac9bdd9febabd0c460380a87e3fe7057
stephan.com <stephan@stephan.com>
2018-12-01 04:24:17 -0500

    100% coverage
    and random order

--- spec/exactinator_spec.rb
+++ spec/exactinator_spec.rb
@@ -8,44 +8,55 @@ RSpec.describe Exactinator do
     menu << Item.new('Chips', '$1')
   end
   let(:total) { 2.95.to_money }
-  let(:exactinator) { Exactinator::Recursive.new(menu, total) }
 
-  it 'should start with no orders' do
-    expect(exactinator).to have(0).orders
+  shared_examples('an exactinator') do
+    it 'should start with no orders' do
+      expect(exactinator).to have(0).orders
+    end
+
+    describe 'results after running' do
+      before do
+        exactinator.run
+      end
+      subject(:results) { exactinator.orders }
+
+      it { is_expected.to be_an_instance_of(Array) }
+      it { is_expected.to all(be_an_instance_of(Order)) }
+      it { is_expected.not_to be_empty }
+      it { is_expected.to have_exactly(1).order }
+
+      describe 'found one order' do
+        subject(:order) { results.first }
+
+        it { is_expected.to have_exactly(1).entries }
+        it 'should have exactly one item' do
+          expect(order.items_count).to eq(1)
+        end
+        it 'should have the right total' do
+          expect(order.total).to eq(total)
+        end
+
+        describe 'with one entry' do
+          subject(:entry) { order.entries.first }
+
+          it 'should have a quantity of one' do
+            expect(entry.quantity).to eq(1)
+          end
+          it 'should be a hamborger' do
+            expect(entry.item).to eq(hamborger)
+          end
+        end
+      end
+    end
   end
 
-  describe 'results after running' do
-    before do
-      exactinator.run
-    end
-    subject(:results) { exactinator.orders }
+  describe Exactinator::Recursive do
+    let(:exactinator) { Exactinator::Recursive.new(menu, total) }
+    it_behaves_like 'an exactinator'
+  end
 
-    it { is_expected.to be_an_instance_of(Array) }
-    it { is_expected.to all(be_an_instance_of(Order)) }
-    it { is_expected.not_to be_empty }
-    it { is_expected.to have_exactly(1).order }
-
-    describe 'found one order' do
-      subject(:order) { results.first }
-
-      it { is_expected.to have_exactly(1).entries }
-      it 'should have exactly one item' do
-        expect(order.items_count).to eq(1)
-      end
-      it 'should have the right total' do
-        expect(order.total).to eq(total)
-      end
-
-      describe 'with one entry' do
-        subject(:entry) { order.entries.first }
-
-        it 'should have a quantity of one' do
-          expect(entry.quantity).to eq(1)
-        end
-        it 'should be a hamborger' do
-          expect(entry.item).to eq(hamborger)
-        end
-      end
-    end
+  describe Exactinator::MonteCarlo do
+    let(:exactinator) { Exactinator::MonteCarlo.new(menu, total) }
+    it_behaves_like 'an exactinator'
   end
 end
